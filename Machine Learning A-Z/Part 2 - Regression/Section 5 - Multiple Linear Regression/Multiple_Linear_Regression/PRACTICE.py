85a38003624d19ef7ef5f638a55beb61a4c3af78
Gurubux <d_gurbux@yahoo.com>
2019-05-11 22:40:51 +0530

    Update PRACTICE.py

--- Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/PRACTICE.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/PRACTICE.py
@@ -26,7 +26,7 @@ X = onehotencoder.fit_transform(X).toarray()
 # Avoiding the Dummy Variable Trap
 X = X[:, 1:]
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
