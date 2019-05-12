def49eb29f79d76c9a6461ce6e3f4b750415eae7
Gurubux <d_gurbux@yahoo.com>
2019-05-12 15:48:51 +0530

    Revert "Backward Elimination"
    This reverts commit 1503d0bc1f8558f8d5c024767624867f8d823d9e.

--- Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/Homework_Solutions/multiple_linear_regression.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/Homework_Solutions/multiple_linear_regression.py
@@ -42,72 +42,19 @@ y_pred = regressor.predict(X_test)
 
 # Building the optimal model using Backward Elimination
 import statsmodels.formula.api as sm
-X1=X
-X = np.append(arr = np.ones((40, 1)).astype(int), values = X_train, axis = 1)
-y1=y
-y=y_train
-
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
 X_opt = X[:, [0, 1, 2, 3, 4, 5]]
 regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
 regressor_OLS.summary()
-print("[0, 1, 2, 3, 4, 5]",["%.3f"%x for x in regressor_OLS.pvalues])
-print("Max PValue ",np.max(regressor_OLS.pvalues))
-
 X_opt = X[:, [0, 1, 3, 4, 5]]
 regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
 regressor_OLS.summary()
-print("[0, 1, 3, 4, 5]",["%.3f"%x for x in regressor_OLS.pvalues])
-print("Max PValue ",np.max(regressor_OLS.pvalues))
 X_opt = X[:, [0, 3, 4, 5]]
 regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
 regressor_OLS.summary()
-print("[0, 3, 4, 5]",["%.3f"%x for x in regressor_OLS.pvalues])
-print("Max PValue ",np.max(regressor_OLS.pvalues))
 X_opt = X[:, [0, 3, 5]]
 regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
 regressor_OLS.summary()
-print("[0, 3, 5]",["%.3f"%x for x in regressor_OLS.pvalues])
-print("Max PValue ",np.max(regressor_OLS.pvalues))
 X_opt = X[:, [0, 3]]
 regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
 regressor_OLS.summary()
-print("[0, 3]",["%.3f"%x for x in regressor_OLS.pvalues])
-print("Max PValue ",np.max(regressor_OLS.pvalues))
-
-X_test = np.append(arr = np.ones((10, 1)).astype(int), values = X_test, axis = 1)
-y_pred_ols = regressor_OLS.predict(X_test[:, [0, 3]])
-
-
-from sklearn.metrics import r2_score,mean_squared_error
-print("LR r2_score-> ",r2_score(y_test,y_pred))
-print("OLS r2_score-> ",r2_score(y_test,y_pred_ols))
-
-
-print("LR MSE-> ",mean_squared_error(y_test,y_pred))
-print("OLS MSE-> ",mean_squared_error(y_test,y_pred_ols))
-
-
-
-
-
-
-"""print('----------------------------')
-print(np.array(xx2_train).reshape(-1,1),y_train)
-print(type(np.array(xx2_train).reshape(-1,1)),type(y_train))
-print(len(np.array(xx2_train).reshape(-1,1)),len(y_train))
-regressor1 = LinearRegression()
-regressor1.fit(np.array(xx2_train).reshape(-1,1), y_train)
-y_pred = regressor1.predict(np.array(xx2).reshape(-1,1))"""
-plt.scatter(X_test[:, [3]], y_test, color = 'red')
-plt.plot(X_test[:, [3]], y_pred, color = 'yellow')
-plt.show()
-
-
-plt.scatter(X1[:, [2]], y1, color = 'black',alpha=0.5,s=100)
-plt.scatter(X_test[:, [3]], y_test, color = 'red')
-plt.scatter(X_test[:, [0]], y_test, color = 'blue')
-plt.plot(X_test[:, [3]], y_pred_ols, color = 'yellow')
-plt.plot(X_test[:, [0]], y_pred_ols, color = 'yellow')
-plt.xlabel('R&D Spend')
-plt.ylabel('Profits')
-plt.show()
